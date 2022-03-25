4a67dbc3dca73bc03067dabd4cf4493fbc5cf1cc
Aysenur <aysenur@gmail.com>
2022-03-26 00:57:20 +0300

    1-2

--- Uygulama-1/application/controllers/Uyeler.php
+++ Uygulama-1/application/controllers/Uyeler.php
@@ -1,32 +1,18 @@
 <?php
-    class uyeler extends CI_Controller{
-        /*Üyeler isimli controller oluşturarak 
-        İçerisindeki uyelistele isimli fonksiyonu oluşturarak dizi içeriğini
-        aşağıdaki gibi ekrana yazdırınız
-        uyeler{
-            id=>1,
-            adi=>"Ahmet",
-            soyadi=>"Gül",
-            email=>asd@assd.com
-        } */
-        
 
-        public function uyelistele(){
-            $uyeler=array(
+class Uyeler extends CI_Controller
+{
+    public function uyegetir()
+    {
+        $uyeler = array(
+            "id"    => "1",
+            "adi"   => "Ayşe",
+            "soyadi" => "Candan",
+            "email" => "asd@ads.com"
+        );
 
-                "id"=>1,
-                "adi"=>'Ahmet',
-                "soyadi"=>'Gül',
-                "email"=>'asaas.com'
-            );
-            echo"<pre>";
-            print_r($uyeler);
-            echo"</pre>";
-
-        }
-
+        echo "<pre>";
+        print_r($uyeler);
+        echo "</pre>";
     }
-
-
-
-?>
+}
--- Uygulama-1/application/controllers/Welcome.php
+++ Uygulama-1/application/controllers/Welcome.php
@@ -1,17 +1,24 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
 	public function index()
 	{
+		echo "Merhaba";
+		die();
 		$this->load->view('welcome_message');
 	}
-	public function merhaba(){
 
-		echo "Merhaba";
+
+	public function listele()
+	{
+		echo "Veriler Listelendi";
 	}
-	public function uyegetir(){
-		echo "uyeler listelendi";
+
+	public function uyegetir()
+	{
+		echo "Üyeler Listelendi";
 	}
 }
